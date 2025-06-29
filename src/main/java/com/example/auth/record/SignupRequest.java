457e638ff0e537ced8739134b5e6e9c0468e9cb0
OMM_DAS <dasomm308@gmail.com>
2025-06-29 23:48:33 +0530

    saved the changes

--- src/main/java/com/example/auth/record/SignupRequest.java
+++ src/main/java/com/example/auth/record/SignupRequest.java
@@ -1,5 +1,26 @@
 package com.example.auth.record;
 
-public record SignupRequest() {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public record SignupRequest(
+		
+		@NotBlank(message="Email cannot be null or empty.")
+		@Pattern(
+		regexp = "^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.(com|in|uk|co)$",
+		flags = Pattern.Flag.CASE_INSENSITIVE,
+	    message = "Email must end with .com, .in, .uk, or .co"
+		)
+		String email,
+		
+		@NotBlank(message="Password cannot be null or empty.")
+		@Size(min=8,max=16,message="Password length should be between 8 to 16")
+		String password,
+		
+		@NotBlank(message="Role cannot be null or empty.")
+		String role
+		
+		) {
 
 }
